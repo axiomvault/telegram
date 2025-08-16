276d2ac8c93e30e35eab9be517c6a12b0221a18c
Axiom <dikshasingh009977@gmail.com>
2025-08-16 17:20:25 +0530

    added api path

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Wallet Generator API</h1>
+      <p>This is a Next.js front-end. Try calling the API from your frontend client.</p>
+    </div>
+  );
+}
